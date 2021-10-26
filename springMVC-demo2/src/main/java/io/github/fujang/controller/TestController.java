67992955a8c726fe1cbe86d0054613748ba72c28
mbfjllybl <mbfjllybl@163.com>
2021-10-26 17:47:51 +0800

    somthing

--- springMVC-demo2/src/main/java/io/github/fujang/controller/TestController.java
+++ springMVC-demo2/src/main/java/io/github/fujang/controller/TestController.java
@@ -2,6 +2,7 @@ package io.github.fujang.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class TestController {
@@ -9,5 +10,10 @@ public class TestController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET, params = "username")
+    public String test() {
+        return "test";
     }
 }
