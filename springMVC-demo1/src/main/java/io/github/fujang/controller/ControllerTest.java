d0bbe90d39671a031ac066cbb45f4cb8c9e098c4
mbfjllybl <mbfjllybl@163.com>
2021-10-26 12:30:02 +0800

    Initial commit

---
+++ springMVC-demo1/src/main/java/io/github/fujang/controller/ControllerTest.java
@@ -0,0 +1,17 @@
+package io.github.fujang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerTest {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/target")
+    public String totarget() {
+        return "target";
+    }
+}
