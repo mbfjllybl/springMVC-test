7af3221da9d5b419b97c295bcacb8ed5c78343f1
mbfjllybl <mbfjllybl@163.com>
2021-10-27 20:51:10 +0800

    complete springMVC RESTFul

--- springMVC-rest/src/main/java/io/github/fujang/controller/TestController.java
+++ springMVC-rest/src/main/java/io/github/fujang/controller/TestController.java
@@ -4,10 +4,31 @@ import io.github.fujang.bean.Employee;
 import io.github.fujang.dao.EmployeeDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Collection;
 
 @Controller
 public class TestController {
 
     @Autowired
     private EmployeeDao employeeDao;
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public String getAllEmployee(Model model) {
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("employees", employees);
+        return "employees";
+    }
+
+
+
+    @RequestMapping(value = "/employee/{id}", method = RequestMethod.DELETE)
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/employee";
+    }
 }
